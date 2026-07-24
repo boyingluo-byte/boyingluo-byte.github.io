9c1b997b82cc82649fb072596ac350d31cff1ddb
boyingluo-byte <boyingluo-byte@users.noreply.github.com>
2026-07-24 15:30:39 +0800

    Site updated: 2026-07-24 15:30:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/07/17/信息收集/"];function toRandomPost(){
+var posts=["2026/07/17/信息收集/","2026/07/24/XSS的挖掘/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
